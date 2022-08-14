6ca324154436184f8665286196efcb2b13e474fe
Juan Pablo Enrici <jpenrici@gmail.com>
2022-08-14 18:37:35 -0300

    New experiment with Godot in 3D.
    Experimental game in development.

---
+++ Pebolim3D_Project/Pebolim3D/scripts/Player_A2.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public class Player_A2 : Spatial
+{
+    float posX;
+    float posY;
+
+    public override void _Ready()
+    {
+        posX = Translation.x;
+        posY = Translation.y;
+    }
+
+    public override void _Process(float delta)
+    {
+        PlayerControl.GetInput(this, new Vector3(posX, posY, Translation.z), false);
+    }
+}
