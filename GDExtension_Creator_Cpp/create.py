173ef85e675353654ad2cfa4be5f6ae0c75450d6
Juan Pablo Enrici <jpenrici@gmail.com>
2023-07-20 11:34:16 -0300

    Update.

--- GDExtension_Creator_Cpp/create.py
+++ GDExtension_Creator_Cpp/create.py
@@ -10,6 +10,7 @@ FULL_PATH = os.path.realpath(__file__)
 PATH, FILENAME = os.path.split(FULL_PATH)
 NOW = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 RESOURCES = "{}/resources".format(PATH)
+GDVERSION = "4.1"
 
 
 def load(path):
@@ -40,87 +41,97 @@ def save(path, text):
 
 def create(directoryName, projectName, gdextensionName):
 
-	# Godot 
-	gdversion = "4.1"
+    # Names
+    projectName = "Demo" if projectName == "" else projectName.strip()
+    directoryName = "GDExtension_Example" if directoryName == "" else directoryName.strip()
+    gdextensionName = "example" if gdextensionName == "" else gdextensionName.strip().replace(' ', '')
+    print("Script: {}\nDate: {}\nDirectory: {}\nProject: {}\nGDExtension: {}".format(FILENAME, NOW, directoryName, projectName, gdextensionName))
 
-	# Names
-	directoryName = "CustomGDExtension" if directoryName == "" else directoryName.strip()
-	projectName = "NewProject" if projectName == "" else projectName.strip()
-	gdextensionName = "customGDExtension" if gdextensionName == "" else gdextensionName.strip().replace(' ', '')
-	print("Script: {}\nDate: {}\nDirectory: {}\nProject: {}\nGDExtension: {}".format(FILENAME, NOW, directoryName, projectName, gdextensionName))
+    # Packages
+    print("Check packages ...")
+    requeriments = ['scons']
+    installed_pkgs_list = sorted([i.key for i in pkg_resources.working_set])
+    for pkg in requeriments:
+        installed = pkg in installed_pkgs_list
+        print("{}: {}".format(pkg, "ok!" if installed else "installation required!"))
+        if not installed:
+            print("Recommended to install packages before continuing!")
+            exit(0)
 
-	# Packages
-	print("Check packages ...")
-	requeriments = ['scons']
-	installed_pkgs_list = sorted([i.key for i in pkg_resources.working_set])
-	for pkg in requeriments:
-		installed = pkg in installed_pkgs_list
-		print("{}: {}".format(pkg, "ok!" if installed else "installation required!"))
-		if not installed:
-			print("Recommended to install packages before continuing!")
-			exit(0)
+    # Directories
+    root = "{}/{}".format(PATH, directoryName)
+    project = "{}/{}".format(root, projectName)
+    godotCpp = "{}/godot-cpp".format(root)
+    repository = "https://github.com/godotengine/godot-cpp"
+    source = "{}/src".format(root)
+    if not os.path.isdir(RESOURCES):
+        print("Resources directory missing!")
+        exit(0)
+    if not os.path.isdir(root):
+        print("Create: {}".format(root))
+        os.mkdir(root)
+    if not os.path.isdir(project):
+        print("Create: {}".format(project))
+        os.mkdir(project)
+    if not os.path.isdir(source):
+        print("Create: {}".format(source))
+        os.mkdir(source)
+    if not os.path.isdir(godotCpp):
+        try:
+            result = input("The godot-cpp library was not found!\nWant to download the repository from GitHub. Yes or No [Y/N]?")
+            if result.upper() == 'Y':
+                print("Download: {}".format(repository))
+                os.system("git clone -b {} {} {}".format(GDVERSION, repository, godotCpp))
+            else:
+                print("\nCopy or enter a path from the godot-cpp library!")
+                print("Change the SConstruct file in the root directory before compiling using SCons!")
+                print("Consult the official documentation of the SCons package!")
+                result = input("Press Enter to continue.")
+        except Exception as e:
+            print("Something went wrong when cloning the godot-cpp repository!")
+            raise e
 
-	# Directories
-	root = "{}/{}".format(PATH, directoryName)
-	project = "{}/{}".format(root, projectName)
-	godotCpp = "{}/godot-cpp".format(root)
-	repository = "https://github.com/godotengine/godot-cpp"
-	source = "{}/src".format(root)
-	if not os.path.isdir(RESOURCES):
-		print("Resources directory missing!")
-		exit(0)
-	if not os.path.isdir(root):
-		print("Create: {}".format(root))
-		os.mkdir(root)
-	if not os.path.isdir(project):
-		print("Create: {}".format(project))
-		os.mkdir(project)
-	if not os.path.isdir(source):
-		print("Create: {}".format(source))
-		os.mkdir(source)
-	if not os.path.isdir(godotCpp):
-		try:
-			print("Download: {}".format(repository))
-			os.system("git clone -b {} {} {}".format(gdversion, repository, godotCpp))
-		except Exception as e:
-			print("Something went wrong when cloning the godot-cpp repository!")
-			raise e
+    # Files
+    paths = [
+        [RESOURCES, "gdexample.h", source, gdextensionName + ".h"],
+        [RESOURCES, "gdexample.cpp", source, gdextensionName + ".cpp"],
+        [RESOURCES, "register_types.h", source, "register_types.h"],
+        [RESOURCES, "register_types.cpp", source, "register_types.cpp"],
+        [RESOURCES, "SConstruct", root, "SConstruct"],
+        [RESOURCES, "message.md", root, "README.md"]
+    ]
+    for pathIn, fileIn, pathOut, fileOut in paths:
+        text = load("{}/{}".format(pathIn, fileIn))
+        text = text.replace("#GDEXTENSION#", gdextensionName)
+        text = text.replace("#GDEXTENSIONH#", gdextensionName.upper())
+        text = text.replace("#GDEXTENSIONCLASS#", gdextensionName.title())
+        text = text.replace("#PROJECT#", projectName)
+        text = text.replace("#ROOT#", root)
+        if not save("{}/{}".format(pathOut, fileOut), text):
+            exit(0)
 
-	# Files
-	actions = [
-		[RESOURCES, "gdexample.h", source, gdextensionName + ".h"],
-		[RESOURCES, "gdexample.cpp", source, gdextensionName + ".cpp"],
-		[RESOURCES, "register_types.h", source, "register_types.h"],
-		[RESOURCES, "register_types.cpp", source, "register_types.cpp"],
-		[RESOURCES, "SConstruct", root, "SConstruct"],
-		[RESOURCES, "message.md", root, "README.md"]
-	]
-	for pathIn, fileIn, pathOut, fileOut in actions:
-		text = load("{}/{}".format(pathIn, fileIn))
-		text = text.replace("#GDEXTENSION#", gdextensionName)
-		text = text.replace("#GDEXTENSIONH#", gdextensionName.upper())
-		text = text.replace("#GDEXTENSIONCLASS#", gdextensionName.title())
-		text = text.replace("#PROJECT#", projectName)
-		text = text.replace("#ROOT#", root)
-		if not save("{}/{}".format(pathOut, fileOut), text):
-			exit(0)
+    # Scons
+    if not os.path.isdir(godotCpp):
+        print("You will need the godot-cpp library in the root directory to finish!")
+        exit(0)
+    try:
+        result = input("Do you want to run Scons? Process can take some time! Yes or No [Y/N]?")
+        if result.upper() == 'Y':
+            os.system("scons -j4 -Q --directory={}".format(root))
+        else:
+            print("Skip Scons ...")
+    except Exception as e:
+        print("Something went wrong when running Scons!")   
 
-	# Scons
-	try:
-		result = input("Do you want to run Scons? Process can take some time! Yes or No [Y/N]?")
-		if result.upper() == 'Y':
-			os.system("scons -j4 --directory={}".format(root))
-		else:
-			print("Skip Scons ...")
-	except Exception as e:
-		print("Something went wrong when running Scons!")	
-
-
-	# Exit
-	print("\n\nRead the \"README\" file in {}".format(root))
+    # Exit
+    print("\n\nRead the \"README\" file in {}\n".format(root))
 
 
 if __name__ == '__main__':
-	# Test
-	create("GDExtension_Example", "Demo", "example")
-	
+    # Test
+    print("Enter the names without special characters and numbers at the beginning!")
+    print("If empty, the default will be used!\n")
+    directoryName = input("Enter the name of the root directory: ")
+    projectName = input("Enter the name of the project Godot: ")
+    gdextensionName = input("Enter the name of the Godot extension in C++: ")
+    create(directoryName, projectName, gdextensionName)
--- GDExtension_Creator_Cpp/resources/message.md
+++ GDExtension_Creator_Cpp/resources/message.md
@@ -1,40 +1,68 @@
-# Project
+# Project GDExtension
+
+This is an example Godot project that uses the C++ language.<br>
+
+## Details
+
+Follow the steps below to finish creating a Godot Project.<br>
 
 1. Open Godot Engine and create a project in:
 #ROOT#/#PROJECT#
 2. Make sure there are no errors when importing the extension ("#GDEXTENSION#").
 3. If necessary, recompile using Scons in:
 #ROOT#
-4. Make sure the extension is accessible from the component panel.
-
-## Details
+4. Make sure the extension is accessible by adding a new node or object ("#GDEXTENSION#").
+5. Modify this README to document your project.
 
 The "#GDEXTENSION#" extension in C++ needs to be implemented to perform actions.
-Learn more at https://docs.godotengine.org/en/stable/tutorials/scripting/gdextension/index.html
+Learn more at https://docs.godotengine.org/en/stable/tutorials/scripting/gdextension/index.html.
+
+## Requeriments
+
+- python
+- scons
+- gcc
+
+## Run
+
+1. Go to the "#ROOT#" directory.
+2. Open the terminal and compile `SConstruct` by running the command `scons -Q`.
+3. Recompile whenever you modify the C++ code in the `\src` directory.
+4. Open the Godot project.
 
 ## Structure
 
-#ROOT#/
-|
-|  # game to test the extension
-+--#PROJECT#/						
-|   |
-|   +--project.godot
-|   |
-|	# GDExtension
-|   +--#GDEXTENSION#.gdextension
-|   |
-|   +--bin/
-|       |
-|       +--libraries	# Built with Scons
-|
-+--godot-cpp/			# C++ bindings
-|
-+--src/         		# Extension source code
-|   |
-|   +--register_types.cpp
-|   +--register_types.h
-|   +--#GDEXTENSION#.cpp
-|   +--#GDEXTENSION#.h
-|
-| Sconstruct		  	# SCons script
+<code>
+#ROOT#/<br>
+|<br>
+|&nbsp;&nbsp;# Game project<br>
++--#PROJECT#/<br>				
+|&nbsp;&nbsp;|<br>
+|&nbsp;&nbsp;+--project.godot<br>
+|&nbsp;&nbsp;|<br>
+|&nbsp;&nbsp;# GDExtension<br>
+|&nbsp;&nbsp;+--#GDEXTENSION#.gdextension<br>
+|&nbsp;&nbsp;|<br>
+|&nbsp;&nbsp;+--bin/<br>
+|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br>
+|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+--libraries&nbsp;# Built with Scons<br>
+|<br>
++--godot-cpp/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;# C++ bindings<br>
+|<br>
++--src/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;# Extension source code<br>
+|&nbsp;&nbsp;&nbsp;|<br>
+|&nbsp;&nbsp;&nbsp;+--register_types.cpp<br>
+|&nbsp;&nbsp;&nbsp;+--register_types.h<br>
+|&nbsp;&nbsp;&nbsp;+--#GDEXTENSION#.cpp<br>
+|&nbsp;&nbsp;&nbsp;+--#GDEXTENSION#.h<br>
+|<br>
+|&nbsp;Sconstruct&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;# SCons script<br>
+</code>
+
+## References
+
+[Godot Engine](https://godotengine.org/) : A free, all-in-one and cross-platform game engine to create 2D and 3D games.<br>
+[GDExtension](https://docs.godotengine.org/en/stable/tutorials/scripting/gdextension/index.html) : Documentation.<br>
+<br>
+[Scons](https://scons.org/) : Open Source software construction tool.<br>
+[Python](https://www.python.org/) : Official site.<br>
